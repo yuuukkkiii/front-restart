3bd70ec5f1d6b8363fece0cf09df15bc6ca60037
zhaihao <zhaihao@maxwell-gp.com.cn>
2024-06-27 16:40:31 +0800

    setup语法糖

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,26 +1,25 @@
 <script lang="ts">
 export default {
-  name: 'Person',
-  setup() {
-    let name = 'bbb'; /*这种写法不是响应式的*/
-    let age = 19;
-    let tel = 120;
+  name: 'Person',//控制组件名
+}
+</script>
 
-    function changeName() {
-      name = 'aaa';
-    }
+<script setup lang="ts">
+let name = 'bbb'; /*这种写法不是响应式的*/
+let age = 19;
+let tel = 120;
+let address = '芦荡路228号';
 
-    function changeAge() {
-      age += 1;
-    }
+function changeName() {
+  name = 'aaa';
+}
 
-    function showTel() {
-      alert(tel);
-    }
+function changeAge() {
+  age += 1;
+}
 
-    return {name, age, changeName, changeAge, showTel}
-    // return ()=>'hello world!'
-  }
+function showTel() {
+  alert(tel);
 }
 </script>
 
@@ -28,6 +27,7 @@ export default {
   <div class="person">
     <h2>姓名：{{ name }}</h2>
     <h2>年龄：{{ age }}</h2>
+    <h2>住址:{{ address }}</h2>
     <button @click="changeName">修改名字</button>
     <button @click="changeAge">修改年龄</button>
     <button @click="showTel">查看联系方式</button>
