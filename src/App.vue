05e7c5ac200f364a7218ad9ed1fb312b5bf4f030
zhaihao <zhaihao@maxwell-gp.com.cn>
2024-07-09 10:52:51 +0800

    监听器监听ref对象

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <script lang="ts">
 import WatchRefObject from "./components/WatchRefObject.vue";
-import Person1 from "./components/Person1.vue";
 import PersonAttribution from "./components/PersonAttribution.vue";
-import PersonComputed from "./components/PersonComputed.vue";
-import PersonWatch from "./components/PersonWatch.vue";
 
 export default {
   name: 'App',
-  components: {WatchRefObject,Person1,PersonComputed,PersonWatch,PersonAttribution} //注册组件
+  components: {WatchRefObject,PersonAttribution} //注册组件
 }
 // import HelloWorld from './components/HelloWorld.vue'
 
--- src/components/WatchRefObject.vue
+++ src/components/WatchRefObject.vue
@@ -6,16 +6,37 @@
     <button @click="changeName">修改姓名</button>
     <button @click="changeAge">修改年龄</button>
     <button @click="changePerson">修改人</button>
+    <h1>情况三：监视【reactive】定义的【对象类型】数据</h1>
+    <h2>姓名：{{ person2.name }}</h2>
+    <h2>年龄：{{ person2.age }}</h2>
+    <h2> 测试：{{ test.a.b.c }}</h2>
+    <button @click="change2Name">修改姓名</button>
+    <button @click="change2Age">修改年龄</button>
+    <button @click="change2Person">修改人</button>
+    <button @click="change2Test">修改测试文本</button>
   </div>
 </template>
 
 
 <script setup lang="ts">/*重命名插件*/
-import {ref, watch} from 'vue'
+import {ref, reactive, watch} from 'vue'
 
 let person = ref({
   name: '张三',
   age: 18
+})
+
+let person2 = reactive({
+  name: '张三',
+  age: 18
+})
+
+let test = reactive({
+  a: {
+    b: {
+      c: 666
+    }
+  }
 })
 
 function changeName() {
@@ -36,6 +57,36 @@ function changePerson() {
 watch(person, (newVal, oldVale) => {
   console.log("打印：", newVal, oldVale)
 }, {deep: true})
+
+
+function change2Name() {
+  person2.name += '-';
+}
+
+function change2Age() {
+  person2.age += 1;
+}
+
+function change2Person() {
+  // 将后面的对象的键值对，赋值给第一个，对象没有发生变化
+  Object.assign(person2, {name: '李四', age: 15});
+}
+
+function change2Test() {
+  test.a.b.c = 888;
+}
+
+// 监视ref对象，监视的是对象的地址值，若想监视对象内部属性的变化，需手动开始深度监视
+// 但是如果只修改属性，新旧值指向的是同一个对象
+// 修改整个对象，old和new才会不同
+watch(person2, (newVal, oldVale) => {
+  console.log("打印：", newVal, oldVale)
+}, {deep: true})
+
+
+watch(test, (newVal, oldVale) => {
+  console.log("打印：", newVal, oldVale)
+})
 </script>
 
 
