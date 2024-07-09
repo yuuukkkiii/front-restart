4478fa8a8d3d97a939281370dceaceb5967d19f9
zhaihao <zhaihao@maxwell-gp.com.cn>
2024-07-09 17:18:20 +0800

    ts的泛型、自定义类型

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,23 @@
 <script lang="ts" setup name="App">
-import WatchRefObject from "./components/base/WatchRefObject.vue";
-import PersonAttribution from "./components/base/PersonAttribution.vue";
-import WatchEffect from "./components/base/WatchEffect.vue";
 import LabelRef from "./components/base/LabelRef.vue";
-import {ref} from "vue";
-
-// import HelloWorld from './components/HelloWorld.vue'
-let title2 = ref();
-let la = ref();
-
-function showLog(){
-  console.log(title2.value);
-  console.log(la.value)
-}
-
+import TSLearn from "./components/base/TSLearn.vue";
+// import {ref} from "vue";
+// // import HelloWorld from './components/HelloWorld.vue'
+// let title2 = ref();
+// let la = ref();
+// function showLog(){
+//   console.log(title2.value);
+//   console.log(la.value)
+// }
 </script>
 
 <template>
-<!--  <Person/>-->
-<!--  <Car/>-->
-<!--  <PersonComputed/>-->
-<!--  <PersonWatch/>-->
-<!--  <PersonAttribution/>-->
-<!--  <Person1/>-->
-<!--  <WatchRefObject/>-->
-<!--  <WatchEffect/>-->
-  <h2 id="title2">hello</h2>
-  <h2 ref="title2">hello</h2>
-  <button @click="showLog">点击</button>
+<!--  <h2 id="title2">hello</h2>-->
+<!--  <h2 ref="title2">hello</h2>-->
+<!--  <button @click="showLog">点击</button>-->
 <!--  ref放在自定义标签上，拿到的是组件的实例对象-->
-  <LabelRef ref="la"/>
+<!--  <LabelRef ref="la"/>-->
+  <TSLearn/>
 </template>
 
 <style scoped>
---
+++ src/components/base/TSLearn.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="person">
+    ???
+  </div>
+</template>
+
+<script setup lang="ts">/*重命名插件*/
+import {ref} from 'vue'
+import {type PersonInter,type Persons} from "../types";
+
+let person: PersonInter = {id: '18238151396', name: '张三', age: 18}
+
+// let personList:PersonInter=[] 这种写法不对
+let personList: Persons = [
+  {id: '18238151396', name: '张三', age: 8},
+  {id: '18238151396', name: '李四', age: 18},
+  {id: '18238151396', name: '王五', age: 23}
+]
+
+</script>
+
+<!--scoped是局部样式-->
+<style scoped>
+
+.person {
+  background-color: skyblue;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 2px;
+}
+
+button {
+  margin: 0 5px;
+}
+</style>
