1562e2f0a7670962c5d3edc9f5c670a42e6a8999
zhaihao <zhaihao@maxwell-gp.com.cn>
2024-07-09 15:43:32 +0800

    标签的ref属性

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
-<script lang="ts">
+<script lang="ts" setup name="App">
 import WatchRefObject from "./components/base/WatchRefObject.vue";
 import PersonAttribution from "./components/base/PersonAttribution.vue";
 import WatchEffect from "./components/base/WatchEffect.vue";
+import LabelRef from "./components/base/LabelRef.vue";
+import {ref} from "vue";
 
-export default {
-  name: 'App',
-  components: {WatchRefObject,PersonAttribution,WatchEffect} //注册组件
-}
 // import HelloWorld from './components/HelloWorld.vue'
+let title2 = ref();
+let la = ref();
+
+function showLog(){
+  console.log(title2.value);
+  console.log(la.value)
+}
 
 </script>
 
@@ -19,7 +24,12 @@ export default {
 <!--  <PersonAttribution/>-->
 <!--  <Person1/>-->
 <!--  <WatchRefObject/>-->
-  <WatchEffect/>
+<!--  <WatchEffect/>-->
+  <h2 id="title2">hello</h2>
+  <h2 ref="title2">hello</h2>
+  <button @click="showLog">点击</button>
+<!--  ref放在自定义标签上，拿到的是组件的实例对象-->
+  <LabelRef ref="la"/>
 </template>
 
 <style scoped>
---
+++ src/components/base/LabelRef.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="person">
+    <h1>中国</h1>
+    <h2 ref="title2">苏州</h2>
+    <h3>吴江区</h3>
+    <button @click="showLog">点我输出h2里的元素</button>
+  </div>
+</template>
+
+<script setup lang="ts">/*重命名插件*/
+import {ref} from 'vue'
+
+// 创建title2，用于存储ref标记的内容
+let title2 = ref();
+let a = ref(0);
+let b = ref(1);
+let c = ref(2);
+
+function showLog() {
+  console.log(title2.value);
+}
+
+// 向父类暴露自己的属性
+defineExpose({a, b, c});
+</script>
+
+<!--scoped是局部样式-->
+<style scoped>
+
+.person {
+  background-color: skyblue;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 2px;
+}
+
+button {
+  margin: 0 5px;
+}
+</style>
